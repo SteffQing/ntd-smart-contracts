bc9fdad1465eb9e309778275f54f84d7ea99ae89
Steve Tomi <steveola23@gmail.com>
2024-04-01 09:06:53 +0100

    Update .gitignore and hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@openzeppelin/hardhat-upgrades");
-require("@nomiclabs/hardhat-ethers");
+// require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
